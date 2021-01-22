f7a3cdeaddfebf62c0a9d50b9e2aac6e003c9201
chenkianwee <chenkianwee@users.noreply.github.com>
2021-01-22 12:44:36 -0500

    initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+=========
+py3dtileslib
+=========
+This project is based on the py3dtiles library by Oslandia available here:
+
+https://gitlab.com/Oslandia/py3dtiles
---
+++ py3dtileslib/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .utils import TileContentReader, convert_to_ecef
+from .tile import TileContent
+from .feature_table import Feature
+from .gltf import GlTF
+from .pnts import Pnts
+from .b3dm import B3dm
+from .batch_table import BatchTable
+from .wkb_utils import TriangleSoup
+from .export import Node
+
+__version__ = '2.0.0'
+__all__ = ['TileContentReader', 'convert_to_ecef', 'TileContent', 'Feature', 'GlTF', 'Pnts',
+           'B3dm', 'BatchTable', 'TriangleSoup', 'Node']
