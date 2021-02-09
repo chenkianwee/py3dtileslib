fix bounding volume computation

py3dtileslib/b3dm.py
@@ -1,8 +1,5 @@
 # coding: utf-8
-import os
-import json
 import struct
-import tempfile
 import numpy as np
 
 from .tile import TileContent, TileHeader, TileBody, TileType
@@ -14,7 +11,7 @@ from . import utils
 class B3dm(TileContent):
 
     @staticmethod
-    def from_glTF(gltf, bt=None):
+    def from_glTF(gltf, bt=None, ft=None):
         """
         Parameters
         ----------
@@ -23,7 +20,10 @@ class B3dm(TileContent):
 
         bt : Batch Table (optional)
             BatchTable object containing per-feature metadata
-
+            
+        ft : Feature Table (optional)
+            FeatureTable object containing per-feature metadata
+            
         Returns
         -------
         tile : TileContent
@@ -32,6 +32,7 @@ class B3dm(TileContent):
         tb = B3dmBody()
         tb.glTF = gltf
         tb.batch_table = bt
+        tb.feature_table = ft
 
         th = B3dmHeader()
         th.sync(tb)
@@ -63,7 +64,6 @@ class B3dm(TileContent):
 
         # build tile body
         b_arr = (array[B3dmHeader.BYTELENGTH:h.tile_byte_length])
-        
         b = B3dmBody.from_array(h, b_arr)
 
         # build TileContent with header and body
@@ -107,7 +107,6 @@ class B3dmHeader(TileHeader):
 
         # extract array
         glTF_arr = utils.glb2arr(body.glTF)
-        # glTF_arr = body.glTF.to_array()
 
         # sync the tile header with feature table contents
         self.tile_byte_length = len(glTF_arr) + B3dmHeader.BYTELENGTH
@@ -122,9 +121,15 @@ class B3dmHeader(TileHeader):
 
             self.tile_byte_length += len(bth_arr)
             self.bt_json_byte_length = len(bth_arr)
-
-        # fth_arr = body.feature_table.header.to_array()
-        # ftb_arr = body.feature_table.body.to_array()
+            
+        if body.feature_table is not None:
+            fth_arr = body.feature_table.header.to_array()
+            ftb_arr = body.feature_table.body.to_array()
+            self.tile_byte_length += len(fth_arr)
+            self.ft_json_byte_length = len(fth_arr)
+            
+            self.tile_byte_length += len(ftb_arr)
+            self.ft_bin_byte_length = len(ftb_arr)
 
     @staticmethod
     def from_array(array):
@@ -150,7 +155,6 @@ class B3dmHeader(TileHeader):
         h.ft_bin_byte_length = struct.unpack("i", array[16:20])[0]
         h.bt_json_byte_length = struct.unpack("i", array[20:24])[0]
         h.bt_bin_byte_length = struct.unpack("i", array[24:28])[0]
-        
         h.type = TileType.BATCHED3DMODEL
 
         return h
@@ -159,14 +163,17 @@ class B3dmHeader(TileHeader):
 class B3dmBody(TileBody):
     def __init__(self):
         self.batch_table = BatchTable()
-        # self.feature_table = FeatureTable()
+        self.feature_table = FeatureTable()
         self.glTF = None
 
     def to_array(self):
-        # TODO : export feature table
         array = utils.glb2arr(self.glTF)
         if self.batch_table is not None:
             array = np.concatenate((self.batch_table.to_array(), array))
+            
+        if self.feature_table is not None:
+            array = np.concatenate((self.feature_table.to_array(), array))
+            
         return array
 
     @staticmethod
@@ -205,8 +212,8 @@ class B3dmBody(TileBody):
 
         # build feature table
         ft_len = th.ft_json_byte_length + th.ft_bin_byte_length
-        # ft_arr = array[0:ft_len]
-        # ft = FeatureTable.from_array(th, ft_arr)
+        ft_arr = array[0:ft_len]
+        ft = FeatureTable.from_array(th, ft_arr)
         
         # build batch table
         bt_json_len = th.bt_json_byte_length
@@ -227,7 +234,7 @@ class B3dmBody(TileBody):
     
         # build tile body with feature table
         b = B3dmBody()
-        # b.feature_table = ft
+        b.feature_table = ft
         b.batch_table = bt
         b.glTF = glTF
 

py3dtileslib/feature_table.py
@@ -112,36 +112,45 @@ class FeatureTableHeader(object):
         # global semantics
         self.points_length = 0
         self.rtc = None
+        
+        #b3dm semantics
+        self.batch_length = None
 
     def to_array(self):
         jsond = self.to_json()
         json_str = json.dumps(jsond).replace(" ", "")
         n = len(json_str) + 28
         json_str += ' ' * (4 - n % 4)
-        return np.frombuffer(json_str.encode('utf-8'), dtype=np.uint8)
-
+        # return np.frombuffer(json_str.encode('utf-8'), dtype=np.uint8)
+        return np.fromstring(json_str, dtype=np.uint8)
+        
     def to_json(self):
         jsond = {}
 
         # length
-        jsond['POINTS_LENGTH'] = self.points_length
+        if self.points_length != 0:
+            jsond['POINTS_LENGTH'] = self.points_length
 
         # rtc
         if self.rtc:
             jsond['RTC_CENTER'] = self.rtc
 
         # positions
-        offset = {'byteOffset': self.positions_offset}
-        if self.positions == SemanticPoint.POSITION:
-            jsond['POSITION'] = offset
-        elif self.positions == SemanticPoint.POSITION_QUANTIZED:
-            jsond['POSITION_QUANTIZED'] = offset
+        if self.positions_offset != 0:
+            offset = {'byteOffset': self.positions_offset}
+            if self.positions == SemanticPoint.POSITION:
+                jsond['POSITION'] = offset
+            elif self.positions == SemanticPoint.POSITION_QUANTIZED:
+                jsond['POSITION_QUANTIZED'] = offset
 
         # colors
         offset = {'byteOffset': self.colors_offset}
         if self.colors == SemanticPoint.RGB:
             jsond['RGB'] = offset
-
+        
+        #batch_length
+        if self.batch_length:
+            jsond['BATCH_LENGTH'] = self.batch_length
         return jsond
 
     @staticmethod
@@ -261,7 +270,13 @@ class FeatureTableHeader(object):
             fth.rtc = jsond['RTC_CENTER']
         else:
             fth.rtc = None
-
+        
+        # RTC (Relative To Center)
+        if "BATCH_LENGTH" in jsond:
+            fth.batch_length = jsond['BATCH_LENGTH']
+        else:
+            fth.batch_length = None
+            
         return fth
 
 
@@ -286,7 +301,9 @@ class FeatureTableBody(object):
         b = FeatureTableBody()
 
         # extract positions
-        b.positions_itemsize = fth.positions_dtype.itemsize
+        if fth.positions_dtype != None:
+            b.positions_itemsize = fth.positions_dtype.itemsize
+            
         b.positions_arr = np.array([], dtype=np.uint8)
 
         if fth.colors_dtype is not None:
@@ -320,10 +337,11 @@ class FeatureTableBody(object):
         npoints = fth.points_length
 
         # extract positions
-        pos_size = fth.positions_dtype.itemsize
-        pos_offset = fth.positions_offset
-        b.positions_arr = array[pos_offset:pos_offset + npoints * pos_size]
-        b.positions_itemsize = pos_size
+        if fth.positions_dtype != None:
+            pos_size = fth.positions_dtype.itemsize
+            pos_offset = fth.positions_offset
+            b.positions_arr = array[pos_offset:pos_offset + npoints * pos_size]
+            b.positions_itemsize = pos_size
 
         # extract colors
         if fth.colors != SemanticPoint.NONE:
@@ -355,9 +373,13 @@ class FeatureTable(object):
         return self.header.points_length
 
     def to_array(self):
+        #TODO FIGURE OUT WHAT IS WRONG WHEN TWO ARRAY CONCATENATE
         fth_arr = self.header.to_array()
         ftb_arr = self.body.to_array()
-        return np.concatenate((fth_arr, ftb_arr))
+        if len(ftb_arr) == 0:
+            return fth_arr
+        else:
+            return np.concatenate((fth_arr, ftb_arr))
 
     @staticmethod
     def from_array(th, array):
@@ -420,3 +442,9 @@ class FeatureTable(object):
         col = self.body.colors(n)
         return Feature.from_array(self.header.positions_dtype, pos,
                                   self.header.colors_dtype, col)
+    
+    def add_batch_length(self, batch_length):
+        self.header.batch_length = batch_length
+    
+    def add_rtc(self, rtc):
+        self.header.rtc = rtc

py3dtileslib/tileset.py
@@ -1,9 +1,5 @@
 import os
 import json
-import math
-import numpy as np
-import geomie3d
-import pyproj
 
 class BoundingBox():
     def __init__(self, minimum, maximum):
@@ -37,16 +33,6 @@ class Tileset(object):
             Node object
         """
         self.root = node
-    
-    def compute_error(self):
-        """
-        This function only works if bbox is already computed
-        """
-        bbox = self.root.bbox
-        lwr_left = [bbox.min[0], bbox.min[1], bbox.min[2]]
-        upr_right =  [bbox.max[0], bbox.max[1], bbox.min[2]]
-        dist = geomie3d.calculate.dist_btw_xyzs(lwr_left, upr_right)
-        self.error = dist
         
     def add_error(self, error):
         """
@@ -83,9 +69,7 @@ class Node(object):
         uniqid : int or str
             Id that is unique to this node.
         """
-        self.geo_loc = None # tuple of lon/lat/alt of the position of the gltf. This location correspond to the origin of the gltf model  
         self.transform = None
-        self.bbox = None
         self.bvol = None
         self.error = None
         self.refine = 'ADD' #other options include "REPLACE"
@@ -102,203 +86,6 @@ class Node(object):
         """
         self.tile_content = tile_content
     
-    def add_geo_loc(self, geo_loc):
-        self.geo_loc = geo_loc
-    
-    def compute_bbox(self):
-        """
-        This function only works if the gltf content is already geo-referenced to epsg:4978, we are assuming the gltf has z-up coordinates while being transform to y-up in the gltf file
-        in the root node of the gltf file. So we need to transform it to y-up, as we need to apply a transformationt to this tile content to z-up in the 3dtile file.
-        """
-        self.bbox = BoundingBox(
-            [float("inf"), float("inf"), float("inf")],
-            [-float("inf"), -float("inf"), -float("inf")])
-        
-        if self.tile_content != None:
-            if self.tile_content.header.magic_value == b"b3dm":
-                gltf = self.tile_content.body.glTF
-                accessors = gltf.accessors
-                meshes = gltf.meshes
-                for mesh in meshes:
-                    prims = mesh.primitives
-                    for prim in prims:
-                        pos = prim.attributes.POSITION
-                        pos_access = accessors[pos]
-                        mx = pos_access.max
-                        mn = pos_access.min
-                        mn_mx = np.array([mn,mx])
-                        content_bbox = BoundingBox(mn_mx[0], mn_mx[1])
-                        self.bbox.add(content_bbox)
-        
-        for c in self.children:
-            c.compute_bbox()
-            self.bbox.add(c.bbox)
-        
-        #change to y-up
-        rot_mat = geomie3d.calculate.rotate_matrice([1,0,0], -90)
-        
-        midpt = self.bbox.center()
-        midpt = [midpt[0],midpt[1], midpt[2]]
-        midpt = self.trsf_pos([midpt], rot_mat)[0]
-        
-        x_half_len = (self.bbox.max[0] - self.bbox.min[0])/2
-        x_axis = [x_half_len, 0, 0]
-        x_axis = self.trsf_pos([x_axis], rot_mat)[0]
-        
-        y_half_len = (self.bbox.max[1] - self.bbox.min[1])/2
-        y_axis = [0, y_half_len, 0]
-        y_axis = self.trsf_pos([y_axis], rot_mat)[0]
-        
-        z_half_len = (self.bbox.max[2] - self.bbox.min[2])/2
-        z_axis = [0, 0, z_half_len]
-        z_axis = self.trsf_pos([z_axis], rot_mat)[0]
-        
-        midpt.extend(x_axis)
-        midpt.extend(y_axis)
-        midpt.extend(z_axis)
-        
-        self.bvol = {'box':midpt}
-        
-    def compute_bregion(self):
-        """
-        This function only works if the gltf content is already geo-referenced to epsg:4978, we are assuming the gltf has z-up coordinates while being transform to y-up in the gltf file
-        in the root node of the gltf file. So we need to transform it to y-up, as we need to apply a transformationt to this tile content to z-up in the 3dtile file.
-        """
-        zup = False
-        self.bbox = BoundingBox(
-            [float("inf"), float("inf"), float("inf")],
-            [-float("inf"), -float("inf"), -float("inf")])
-        
-        if self.tile_content != None:
-            if self.tile_content.header.magic_value == b"b3dm":
-                gltf = self.tile_content.body.glTF
-                accessors = gltf.accessors
-                meshes = gltf.meshes
-                nodes = gltf.nodes
-                for node in nodes:
-                    mat = node.matrix
-                    if mat != None:
-                        if mat == [1, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1]:
-                            #the original coordinate of the gltf model is assumed to be z-up
-                            zup = True
-                    
-                for mesh in meshes:
-                    prims = mesh.primitives
-                    for prim in prims:
-                        pos = prim.attributes.POSITION
-                        pos_access = accessors[pos]
-                        mx = pos_access.max
-                        mn = pos_access.min
-                        mn_mx = np.array([mn,mx])
-                        content_bbox = BoundingBox(mn_mx[0], mn_mx[1])
-                        self.bbox.add(content_bbox)
-        else:
-            zup = True
-        
-        for c in self.children:
-            c.compute_bregion()
-            self.bbox.add(c.bbox)
-        
-        epsg4326_4978 = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:4978", always_xy = True)
-        epsg4978_4979 = pyproj.Transformer.from_crs("EPSG:4978", "EPSG:4979", always_xy = True)
-        
-        mnx = self.bbox.min[0]
-        mny = self.bbox.min[1]
-        mnz = self.bbox.min[2]
-        
-        mxx = self.bbox.max[0]
-        mxy = self.bbox.max[1]
-        mxz = self.bbox.max[2]
-        
-        trsf_mat = geomie3d.calculate.rotate_matrice((1,0,0), 0)
-        
-        if zup == False:
-            trsf_mat = geomie3d.calculate.rotate_matrice((1,0,0), -90)
- 
-        geo_loc = self.geo_loc
-        geo_loc_xyz = epsg4326_4978.transform(geo_loc[0], geo_loc[1], geo_loc[2])
-        trsl_mat = geomie3d.calculate.translate_matrice(geo_loc_xyz[0], geo_loc_xyz[1], geo_loc_xyz[2])
-        trsf_mat = trsf_mat@trsl_mat
-        
-        rad_list = []
-        rad = True
-        y_half_len = (mxy - mny)/2
-        midy = mny + y_half_len
-        west = (mnx, midy, mnz)
-        west_mx = (mnx, midy, mxz)
-        west = self.trsf_pos([west], trsf_mat)[0]
-        west_mx = self.trsf_pos([west_mx], trsf_mat)[0]
-        west_rad = epsg4978_4979.transform(west[0], west[1], west[2], radians = rad)
-        west_rad_mx = epsg4978_4979.transform(west[0], west[1], west_mx[2], radians = rad)
-        rad_list.append(list(west_rad))
-        rad_list.append(list(west_rad_mx))
-        
-        x_half_len = (mxx - mnx)/2
-        midx = mnx + x_half_len
-        south = (midx, mny, mnz)
-        south_mx = (midx, mny, mxz)
-        south = self.trsf_pos([south], trsf_mat)[0]
-        south_mx = self.trsf_pos([south_mx], trsf_mat)[0]
-        south_rad = epsg4978_4979.transform(south[0], south[1], south[2], radians = rad)
-        south_rad_mx = epsg4978_4979.transform(south[0], south[1], south_mx[2], radians = rad)
-        rad_list.append(list(south_rad))
-        rad_list.append(list(south_rad_mx))
-        
-        east = (mxx, midy, mnz)
-        east_mx = (mxx, midy, mxz)
-        east = self.trsf_pos([east], trsf_mat)[0]
-        east_mx = self.trsf_pos([east_mx], trsf_mat)[0]
-        east_rad = epsg4978_4979.transform(east[0], east[1], east[2], radians = rad)
-        east_rad_mx = epsg4978_4979.transform(east[0], east[1], east_mx[2], radians = rad)
-        rad_list.append(list(east_rad))
-        rad_list.append(list(east_rad_mx))
-        
-        north = (midx, mxy, mnz)
-        north_mx = (midx, mxy, mxz)
-        north = self.trsf_pos([north], trsf_mat)[0]
-        north_mx = self.trsf_pos([north_mx], trsf_mat)[0]
-        
-        north_rad = epsg4978_4979.transform(north[0], north[1], north[2], radians = rad)
-        north_rad_mx = epsg4978_4979.transform(north[0], north[1], north_mx[2], radians = rad)
-        rad_list.append(list(north_rad))
-        rad_list.append(list(north_rad_mx))
-        
-        zip_rad = zip(*rad_list)
-        z_val = list(zip_rad)[2]
-        mnz_4979 = min(z_val)
-        mxz_4979 = max(z_val)
-        
-        # print('west', west_rad)
-        # print('south', south_rad)
-        # print('east', east_rad)
-        # print('north', north_rad)
-        
-        region = [west_rad[0], south_rad[1], east_rad[0], north_rad[1], mnz_4979, mxz_4979]
-        self.bvol = {'region':region}
-    
-    def trsf_pos(self, pos_list, trsf_mat):
-        if type(pos_list) != np.ndarray:
-            np.array(pos_list)
-        
-        #add an extra column to the points
-        npos = len(pos_list)
-        xyzw = np.ones((npos,4))
-        xyzw[:,:-1] = pos_list
-        t_xyzw = np.dot(xyzw, trsf_mat.T)
-        trsf_xyzs = t_xyzw[:,:-1]
-        
-        return trsf_xyzs.tolist()
-    
-    def compute_error(self):
-        """
-        This function only works if bbox is already computed
-        """
-        bbox = self.bbox
-        lwr_left = [bbox.min[0], bbox.min[1], bbox.min[2]]
-        upr_right =  [bbox.max[0], bbox.max[1], bbox.min[2]]
-        dist = geomie3d.calculate.dist_btw_xyzs(lwr_left, upr_right)
-        self.error = dist
-    
     def add_box(self, box):
         """
         Parameters
@@ -312,44 +99,18 @@ class Node(object):
         """
         self.bvol = {'box': box}
         
-    
-    def compute_enu2fixframe(self):
+    def add_bregion(self, region):
         """
-        Compute the East North Up (ENU) coordinate to ECEF 4x4 transformation
-        https://gis.stackexchange.com/questions/308445/local-enu-point-of-interest-to-ecef
+        Parameters
+        ----------
+        region : Tuple
+            Tuple specifying either box (https://github.com/CesiumGS/3d-tiles/tree/master/specification#bounding-volumes)
+            e.g. bbox [0,0,0,
+                       100,0,0,
+                       0,100,0,
+                       0,0,10]
         """
-        lla_origin = self.geo_loc
-        epsg4326_4978 = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:4978", always_xy = True)
-        ecef_origin = epsg4326_4978.transform(lla_origin[0], lla_origin[1], lla_origin[2])
-            
-        lon = math.radians(lla_origin[0])
-        lat = math.radians(lla_origin[1])
-        sin_lon = math.sin(lon)
-        sin_lat = math.sin(lat)
-        cos_lon = math.cos(lon)
-        cos_lat = math.cos(lat)
-        
-        r1 = sin_lon*-1
-        r2 = (sin_lat*-1)*cos_lon
-        r3 = cos_lat*cos_lon
-        r4 = cos_lon
-        r5 = (sin_lat*-1)*sin_lon
-        r6 = cos_lat*sin_lon
-        r7 = 0
-        r8 = cos_lat
-        r9 = sin_lat
-        
-        tx = ecef_origin[0]
-        ty = ecef_origin[1]
-        tz = ecef_origin[2]
-        
-        trsf_mat = np.array([[r1,r2,r3,tx],
-                            [r4,r5,r6,ty],
-                            [r7,r8,r9,tz],
-                            [0,0,0,1]])
-        
-        trsf_mat_flat = trsf_mat.T.flatten().tolist()
-        self.transform = trsf_mat_flat
+        self.bvol = {'region': region}
     
     def add_transform(self, transform):
         """
@@ -394,7 +155,6 @@ class Node(object):
         node_dict = {}
         node_dict['boundingVolume'] = self.bvol
         node_dict['geometricError'] = self.error
-        
         if self.transform != None:
             node_dict['transform'] = self.transform
         if self.tile_content != None:
@@ -413,9 +173,7 @@ class Node(object):
         if magic == b"b3dm":
             filename = str(self.uniqid)+'.b3dm'
             file_path = os.path.join(tile_path, filename)
-            f = open(file_path, 'wb')
-            f.write(content.to_array())
-            f.close()
+            content.save_as(file_path)
             
             gltf = self.tile_content.body.glTF
             gltf_path = os.path.join(tile_path, str(self.uniqid)+'.glb')